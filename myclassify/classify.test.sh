2d9b6a982850854c4b8f98cf9e39afeeeb043db5
Xiaoxiao Wang <xiaoxiaowang@Xiaoxiaos-MacBook-Pro.local>
2017-05-04 20:45:30 -0500

    basic classifiers search parameters

--- myclassify/classify.test.sh
+++ myclassify/classify.test.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
-for filename in test_res/*.txt; do
+
+echo "Classifier: $1"
+for filename in test_res/$1-dev/*.txt; do
+    ##### convert the output to TREC format #####
+    name="${filename%.*}"
+    TREC_DATA="$name.trec_eval"
+    perl convertWekaOutputToTRECFormat.pl $filename test_res/dev-less-than-40.xml > $TREC_DATA 2>/dev/null
+
+    ##### evaluate #####
+    echo "TREC Scores of $name:"
+    TREC_OUT="$name.trec_out"
+    trec_eval-8.0/trec_eval -q -c ./eval/Dev-T40.judgment $TREC_DATA > $TREC_OUT
+    grep -E -w 'map.*all|recip_rank.*all' $TREC_OUT
+
+done
+
+for filename in test_res/$1-test/*.txt; do
     ##### convert the output to TREC format #####
     name="${filename%.*}"
     TREC_DATA="$name.trec_eval"
